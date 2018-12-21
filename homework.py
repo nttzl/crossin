a3a09943d763f3ae5ddf8c17f17cc077cc9aac47
nttzl <33371278+nttzl@users.noreply.github.com>
2018-12-21 21:00:01 +0800

    Update homework.py

--- homework.py
+++ homework.py
@@ -6,6 +6,8 @@ f.seek(0)
 lines = f.readlines()
 n = len(lines)
 words = [line.split("\t")[0] for line in lines if line.strip()]
+key = "0B1BB61A44BE91B250ADFB416F2AD53B"
+url = "http://dict-co.iciba.com/api/dictionary.php?w={}&key=" + key
 
 while 1:
     word=input("请输入你要记录的单词(直接回车退出程序):").strip()
@@ -14,6 +16,8 @@ while 1:
     if word in words:
         print("单词已存在")
     else:
+        r = request(url.format(word))
+        print(r.text)
         worditem = word + '\t' + str(datetime.date.today())
         print(worditem)
         f.write(worditem + '\n')
